dc8c13086cdf01c787f1ba123c5df3c03c3ac5d0
Ahmedalla1144 <ahmed.alla56756@gmail.com>
2024-05-03 08:31:47 +0300

    revalidate

--- src/app/lib/getProducts.tsx
+++ src/app/lib/getProducts.tsx
@@ -1,5 +1,5 @@
 export default async function getProducts() {
   const api = "https://dummyjson.com/products";
-  const res = await fetch(api);
+  const res = await fetch(api, { next: { revalidate: 3600}});
   return res.json();
 }
