6c0e2238ec400bf2d11de22ceb3018686b70be13
Ahmedalla1144 <107187857+Ahmedalla1144@users.noreply.github.com>
2025-08-06 11:47:26 +0300

    Update getProducts.tsx

--- src/app/lib/getProducts.tsx
+++ src/app/lib/getProducts.tsx
@@ -1,5 +1,5 @@
 export default async function getProducts() {
-  const api = "https://dummyjson.com/products";
+  const api = "https://dummyjson.com/products?limit=200";
   const res = await fetch(api, { next: { revalidate: 3600}});
   return res.json();
 }
